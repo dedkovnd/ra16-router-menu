8e6edb796a2eaa491cf5f9dafae69443e110e806
dedkovnd <dedkovnd@gmail.com>
2020-10-06 20:17:30 +0300

    init commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {HomePage} from "./HomePage";
+import {DriftPage} from "./DriftPage";
+import {ForzaPage} from "./ForzaPage";
+import {TimeAttackPage} from "./TimeAttackPage";
+import {Menu} from "./Menu";
+
+function App() {
+  return (
+      <Router>
+        <div>
+          <Menu />
+          <div className="page">
+            <Route path="/" exact component={HomePage} />
+            <Route path="/drift" component={DriftPage} />
+            <Route path="/timeattack" component={TimeAttackPage} />
+            <Route path="/forza" component={ForzaPage} />
+          </div>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
---
+++ src/Menu.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Menu() {
+    return (
+        <nav className="menu">
+            <a className="menu__item" href="/">Главная</a>
+            <a className="menu__item" href="/drift">Дрифт-такси</a>
+            <a className="menu__item" href="/timeattack">Time Attack</a>
+            <a className="menu__item" href="/forza">Forza Karting</a>
+        </nav>
+    )
+}
